645fefe1ce591b180049010548ad3c5d9bd06ff6
Ayush Mishra <117655349+ayush-mishra7@users.noreply.github.com>
2025-12-28 22:27:22 +0530

    Time: 25 ms (97.45%) | Memory: 100.7 MB (31.90%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class RandomizedSet {
+
+    private List<Integer> list;
+    private Map<Integer, Integer> map;
+    private Random random;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+
+        // Move last element to the index of element to remove
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+
+        // Remove last element
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+        int randomIndex = random.nextInt(list.size());
+        return list.get(randomIndex);
+    }
+}
+
