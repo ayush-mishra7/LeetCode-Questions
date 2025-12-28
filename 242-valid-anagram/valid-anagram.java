88315393c49768741010796bc43135055395a272
Ayush Mishra <117655349+ayush-mishra7@users.noreply.github.com>
2025-12-28 21:41:38 +0530

    Time: 2 ms (99.42%) | Memory: 45 MB (51.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,12 +1,24 @@
+import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+        // n log n
+        // char[] a = s.toCharArray();
+        // char[] b = t.toCharArray();
+
+        // Arrays.sort(a);
+        // Arrays.sort(b);
+        
+        // return Arrays.equals(a, b);
+        
+        // O(n)
         int[] freq = new int[26];
+
         for(char c:s.toCharArray()) freq[c-'a']++;
         for(char c:t.toCharArray()) freq[c-'a']--;
-
-        for(int x: freq){
-            if(x!=0) return false;
-        }
+        for(int x:freq){
+            if(x != 0) return false;
+        } 
         return true;
     }
 }
