813d5cf94d13734bb0632192e545815ad132792f
Ayush Mishra <117655349+ayush-mishra7@users.noreply.github.com>
2026-02-10 08:18:47 +0530

    Time: 375 ms (31.75%) | Memory: 46.6 MB (98.36%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -5,19 +5,23 @@ class Solution {
         int n = nums.length;
         int ans = 0;
 
+        // Fix the left boundary of the subarray
         for (int i = 0; i < n; i++) {
             Set<Integer> evenSet = new HashSet<>();
             Set<Integer> oddSet = new HashSet<>();
 
+            // Expand the right boundary
             for (int j = i; j < n; j++) {
                 int x = nums[j];
 
+                // Track distinct evens and odds in the current subarray
                 if (x % 2 == 0) {
                     evenSet.add(x);
                 } else {
                     oddSet.add(x);
                 }
 
+                // Balanced subarray condition
                 if (evenSet.size() == oddSet.size()) {
                     ans = Math.max(ans, j - i + 1);
                 }
